58bdc5bd15ed8b727fa3920c4bd5698802499ec0
youjaebeom <dbwo4011@gmail.com>
2020-07-14 03:13:28 +0900

    Merge sort in-place sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,42 +1,68 @@
 import numpy as np
 print("Merge sort")
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
 
-def mergesort(arr,start,end):
-    print("start:", start, "mid",  int((start + end) / 2), "end:", end)
-    print(arr[start:end])
-    if(start<end-1):
-        mid = int((start + end) / 2)
-        leftarr=mergesort(arr, start, mid)
-        rightarr=mergesort(arr, mid, end)
-        return sort(leftarr,rightarr)
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
 
-def sort(left,right):
-    sortarr=[]
-    i=0
-    j=0
-    print("left:",left,"right:",right)
-    while(i<len(left)) & (j<len(right)):
-        if left[i]<right[j]:
-            sortarr.append(left[i])
-            i+=1
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l += 1
         else:
-            sortarr.append(right[j])
-            j+=1
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
 
-    ## 비교하고 sort하고 나서 혹시 남은 배열처리
-    while i<len(left) :
-        sortarr.append(left[i])
-        i+=1
-    while j<len(right):
-        sortarr.append(right[j])
-        j+=1
-    print("\nsortarr :",sortarr,"\n")
-    return sortarr
+def mergesort(arr):
+    def sort(start,end):
+        print("start:", start, "mid",  int((start + end) / 2), "end:", end)
+        print(arr[start:end])
+        if end - start < 2:
+            return
+        mid = int((start + end) / 2)
+        sort(start,mid)
+        sort(mid,end)
+        temp=merge(start,mid,end)
+        return temp
+    def merge(start,mid,end):
+        temp = []
+        l, h = start, mid
+
+        while l < mid and h < end:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < end:
+            temp.append(arr[h])
+            h += 1
+
+        for i in range(start, end):
+            arr[i] = temp[i - start]
+        print("tmp",temp)
+        return temp
+    return sort(0, len(arr))
+
 
 
 
 arr=[37,10,22,30,35,13,25,24]
-start=0
-end=len(arr)
-result=mergesort(arr,start,end)
+result=merge_sort(arr)
+result1=mergesort(arr)
+
 print("result : ",result)
+print("result1 :",result1)
